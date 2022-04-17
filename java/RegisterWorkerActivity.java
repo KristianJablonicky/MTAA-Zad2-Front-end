08ee5636fa2844d9d5ed8f9538970bd16b759c05
KristianJablonicky <71824623+KristianJablonicky@users.noreply.github.com>
2022-04-17 16:54:54 +0200

    Update and rename RegisterActivity.java to RegisterWorkerActivity.java

---
+++ java/RegisterWorkerActivity.java
@@ -0,0 +1,70 @@
+package mtaa.java;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterWorkerActivity extends AppCompatActivity {
+
+    public void popupMessage(String nadpis, String sprava){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(sprava);
+        alertDialogBuilder.setTitle(nadpis);
+        alertDialogBuilder.setNegativeButton("ok", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d(nadpis,"Ok button stlaceny");
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_worker);
+
+        Button registerTlacidlo = (Button)findViewById(R.id.button_createWorker);
+        registerTlacidlo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlString = "/postWorker/W/";
+
+                EditText menoInput = (EditText) findViewById(R.id.textInput_menoWorker);
+                String meno = menoInput.getText().toString();
+                if(meno.length() < 1){
+                    popupMessage("Chyba!", "Údaj Meno musí byť vlpnený");
+                }
+                else{
+                    urlString += "name=" + meno + "/";
+                }
+
+                EditText hesloInput = (EditText) findViewById(R.id.textInput_hesloWorker);
+                String heslo = hesloInput.getText().toString();
+                if(heslo.length() < 1){
+                    popupMessage("Chyba!", "Údaj Meno musí byť vlpnený");
+                }
+                else{
+                    urlString += "password=" + heslo + "/";
+                }
+
+                volania objekt = new volania();
+                objekt.postWorker(urlString);
+
+                Log.i("ok",meno);
+
+            }
+        });
+    }
+}
