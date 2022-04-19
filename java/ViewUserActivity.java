db1cec9f788bfc543b59335a34579d4f92a2624b
KristianJablonicky <71824623+KristianJablonicky@users.noreply.github.com>
2022-04-19 23:47:32 +0200

    Add files via upload

---
+++ java/ViewUserActivity.java
@@ -0,0 +1,106 @@
+package mtaa.java;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+
+import mtaa.java.data.User;
+
+public class ViewUserActivity extends AppCompatActivity {
+
+    public void popupMessage(String nadpis, String sprava){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(sprava);
+        alertDialogBuilder.setTitle(nadpis);
+        alertDialogBuilder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Log.d(nadpis,"Ok button stlaceny");
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_view_profile);
+
+            User u = (User) extras.get("currentUser");
+
+
+            JSONObject pouzivatel = Requests.GET_request("/get" + extras.get("type") + "/" + extras.get("id") + "/");
+            Log.i("URL", "/get" + extras.get("type") + "/" + extras.get("id") + "/");
+
+            TextView meno = (TextView) findViewById(R.id.text_view_meno);
+            TextView email = (TextView) findViewById(R.id.text_view_email);
+            TextView date = (TextView) findViewById(R.id.text_view_date);
+            TextView phone = (TextView) findViewById(R.id.text_view_phone);
+            TextView company = (TextView) findViewById(R.id.text_view_company);
+
+            if(pouzivatel != null) {
+                try {
+                    User viewCiel = new User(pouzivatel);
+
+                    meno.setText(viewCiel.getName());
+                    if(viewCiel.getEmail() != null)
+                        email.setText(viewCiel.getEmail());
+                    if(viewCiel.getPhone() != null)
+                        phone.setText(viewCiel.getPhone());
+                    if(viewCiel.getCompanyID() != -1) {
+                        JSONObject spolocnost = Requests.GET_request("/getCompany/" + extras.get("id") + "/");
+                        String spolocnostString = (String) spolocnost.get("name");
+                        Log.i("Spolocnost", spolocnostString);
+                        company.setText(spolocnostString);
+                    }
+                    if (viewCiel.getBirthday() != null) {
+
+                        Format formatter = new SimpleDateFormat("yyyy-MM-dd");
+                        String dateString = formatter.format(viewCiel.getBirthday());
+                        date.setText(dateString);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                Log.i("Error", "pouzivatel s danym ID neexistuje");
+                return;
+            }
+
+            Button pdfTlacidlo = (Button) findViewById(R.id.button_view_PDF);
+            pdfTlacidlo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(u.getCompanyID() == -1) {
+                        popupMessage("INFO", "Používateľ nemá zadaný životopis");
+                    }
+                }
+            });
+
+        }
+        else
+            Log.i("Error", "extras = null");
+    }
+
+}
