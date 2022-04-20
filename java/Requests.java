d9b4ca83adab75e90847abc40e9c752a7acb1ff1
KristianJablonicky <71824623+KristianJablonicky@users.noreply.github.com>
2022-04-20 08:51:22 +0200

    Update Requests.java

--- java/Requests.java
+++ java/Requests.java
@@ -1,27 +1,34 @@
 package mtaa.java;
 
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.StrictMode;
 import android.util.Log;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.annotation.RequiresApi;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class Requests {
-    static private String IP = "10.10.37.255";   //MATUS-IP
-    //static private String IP = "192.168.219.127"; //KRISTIAN-IP
+    //static private String IP = "10.10.37.255";   //MATUS-IP
+    static private String IP = "192.168.219.127"; //KRISTIAN-IP
     static private String PORT = ":8000";
 
     //https://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java
@@ -192,35 +199,72 @@ class Requests {
         return code;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public int PDF_request(String type, String urlBody) throws SecurityException{
 
-    /*
-    public void postWorker(String urlString){
+        //https://stackoverflow.com/questions/2469451/upload-files-from-java-client-to-a-http-server
+        String boundary = Long.toHexString(System.currentTimeMillis());
+        String charset = "UTF-8";
+        String CRLF = "\r\n";
 
+        //Path path = FileSystems.getDefault().getPath("sdcard/Download", "zivotopis.pdf");
+        //"/sdcard/Download/zivotopis.pdf"
+
+        Path path = Paths.get("/sdcard/Download/zivotopis.pdf");
+        //Path path = Paths.get("/storage/emulated/0/Download/cv-1.pdf");
+        File binaryFile = new File(String.valueOf(path));
+
+        binaryFile.setReadable(true, false);
+
+        boolean exists = binaryFile.exists();
+        if (exists == true) {
+
+            // Printing the permissions associated
+            // with the file
+            Log.i("Executable: ", String.valueOf(binaryFile.canExecute()));
+            Log.i("Readable: ", String.valueOf(binaryFile.canRead()));
+            Log.i("Writable: ", String.valueOf(binaryFile.canWrite()));
+        }
+
+
+        URLConnection connection = null;
         try {
-            Log.i("url", "http://" + IP + urlString);
-            URL url = new URL("http://" + IP + urlString);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-            //https://stackoverflow.com/questions/4205980/java-sending-http-parameters-via-post-method-easily
-            con.setDoOutput( true );
-            con.setInstanceFollowRedirects( false );
-            con.setRequestMethod("POST");
-
-            //-----------NEPOSIELA ZIADNE REQUESTY, A TEXTOVE POLIA SA ESTE NENACITAVAJU--------------
-
-            Log.i("ok?","ok");
-
-        } catch (MalformedURLException e) {
+            connection = new URL("http://" + IP + PORT + urlBody).openConnection();
+        } catch (IOException e) {
             e.printStackTrace();
-        } catch (ProtocolException e) {
+        }
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+        try (
+                OutputStream output = connection.getOutputStream();
+                PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+        ) {writer.append("--" + boundary).append(CRLF);
+            writer.append("Content-Disposition: form-data; name=\"binaryFile\"; filename=\"" + binaryFile.getName() + "\"").append(CRLF);
+            writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(binaryFile.getName())).append(CRLF);
+            writer.append("Content-Transfer-Encoding: binary").append(CRLF);
+            writer.append(CRLF).flush();
+            Files.copy(binaryFile.toPath(), output);
+
+            output.flush(); // Important before continuing with writer!
+            writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+            // End of multipart/form-data.
+            writer.append("--" + boundary + "--").append(CRLF).flush();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        try {
+            int responseCode = ((HttpURLConnection) connection).getResponseCode();
+            return responseCode;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 400;
+        }
 
 
     }
-
-     */
 }
