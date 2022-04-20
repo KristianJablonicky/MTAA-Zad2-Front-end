45dbfea76ac7f79d42c5ade6130d9147dc5e3920
KristianJablonicky <71824623+KristianJablonicky@users.noreply.github.com>
2022-04-20 06:46:20 +0200

    Update MainActivity.java

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         registerTlacidlo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                startActivity(new Intent(MainActivity.this, RegisterWorkerActivity.class));
             }
         });
 
